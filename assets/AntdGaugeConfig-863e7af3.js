741990a78e68b57183721a12b747e13742918aea
xiaopu <1182810784@qq.com>
2023-11-19 11:15:25 +0800

    update publish vite

--- assets/AntdGaugeConfig-863e7af3.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as r}from"./index-c0a2bb80.js";class s extends e.Component{render(){return r.jsx(r.Fragment,{children:"仪表盘配置"})}}export{s as AntdGaugeConfig};
---
+++ assets/AntdLiquidConfig-35828884.js
@@ -0,0 +1,1 @@
+import{r as e,j as r}from"./index-17e19b3a.js";class s extends e.Component{render(){return r.jsx(r.Fragment,{children:"水波配置"})}}export{s as AntdLiquidConfig};
