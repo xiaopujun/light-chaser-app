a63c2bc04244bcf555ba698b06cc34d00440692c
dagu <1182810784@qq.com>
2024-06-04 20:38:50 +0800

    更新默认项目

---
+++ assets/Demo-NTy_gnPU.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-OsS9UfkW.js";function r(){return t.jsx("div",{})}export{r as default};
---
+++ assets/Down-uvD6wECP.js
@@ -0,0 +1,1 @@
+import{R as t}from"./index-OsS9UfkW.js";import{I as o}from"./index-K2O65-hP.js";const r=o("down",!1,function(e){return t.createElement("svg",{width:e.size,height:e.size,viewBox:"0 0 48 48",fill:"none"},t.createElement("path",{d:"M36 18L24 30L12 18",stroke:e.colors[0],strokeWidth:e.strokeWidth,strokeLinecap:e.strokeLinecap,strokeLinejoin:e.strokeLinejoin}))});export{r as D};
